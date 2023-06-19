438b2a725de1562ffc559b356543b40d0094ae22
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2023-06-19 17:33:04 -0400

    def options(self, request, *args, **kwargs):

--- View/views.py
+++ View/views.py
@@ -103,4 +103,14 @@ def __init__(self, **kwargs):
 
 
 
-
+def options(self, request, *args, **kwargs):
+    """ Handle responding to requests for the OPTIONS HTTP verb."""
+    response = HttpResponse()
+    response.headers["Allow"] = ", ".join(self._allowed_methods())
+    response.headers["Content-Length"] = "0"
+    if self.view_is_async:
+        async def func():
+            return response
+        return func()
+    else:
+        return response
