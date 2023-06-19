309a9c5c45befcd8c04df40e8f3d75ea304da628
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2023-06-19 17:19:06 -0400

    def dispatch(self, request, *args, **kwargs):

--- View/views.py
+++ View/views.py
@@ -55,3 +55,16 @@ def as_view(cls, **initkwargs):
     return view
 
 
+
+
+def dispatch(self, request, *args, **kwargs):
+    # Try to dispatch to the right method; if a method doesn't exist,
+    # defer to the error handler. Also defer to the error handler if the
+    # request method isn't on the approved list.
+    if request.method.lower() in self.http_method_names:
+        handler = getattr(
+            self, request.method.lower(), self.http_method_not_allowed
+        )
+    else:
+        handler = self.http_method_not_allowed
+    return handler(request, *args, **kwargs)
