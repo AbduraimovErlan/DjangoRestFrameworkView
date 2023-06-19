17de8c40de2633026eb3b26463992987a8834bec
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2023-06-19 17:24:13 -0400

    def http_method_not_allowed(self, request, *args, **kwargs):

--- View/views.py
+++ View/views.py
@@ -68,3 +68,24 @@ def dispatch(self, request, *args, **kwargs):
     else:
         handler = self.http_method_not_allowed
     return handler(request, *args, **kwargs)
+
+
+
+
+def http_method_not_allowed(self, request, *args, **kwargs):
+    logger.warning(
+        "Method Not Allowed (%s): %s",
+        request.method,
+        request.path,
+        extra={"status_code": 405, "request": request},
+    )
+    response = HttpResponseNotAllowed(self._allowed_methods())
+    if self.veiw_is_async:
+        async def func():
+            return response
+        return func()
+    else:
+        return response
+
+
+
