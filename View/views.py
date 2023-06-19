22069704cedfda39c543c09740d5a9226dd093e6
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2023-06-19 17:28:18 -0400

    def __init__(self, **kwargs):

--- View/views.py
+++ View/views.py
@@ -89,3 +89,18 @@ def http_method_not_allowed(self, request, *args, **kwargs):
 
 
 
+
+def __init__(self, **kwargs):
+    """
+    Constructor. Called in the URLconf; can contain helpful extra
+    keyword arguments, and other things.
+    """
+    # Go through keyword arguments, and either save their values to our
+    # instance, or raise an error.
+    for key, value in kwargs.items():
+        setattr(self, key, value)
+
+
+
+
+
